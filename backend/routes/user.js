68bd1c0095a3224ff7ca12f880f7bb4b54a403ae
Bilal <crediblebilal@gmail.com>
2023-11-09 13:01:08 +0530

    added / route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,6 +5,7 @@ const { isLogin } = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/', userController.hello);
+router.get('/abc', userController.hello);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/addnickname', userController.addNickname);
